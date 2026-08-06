90c8cad3a767892c08f049277cd9f225c62d8e19
FanNeckIn <157160710+FanNeckIn@users.noreply.github.com>
2026-08-07 00:00:41 +0300

    E

--- characters.py
+++ characters.py
@@ -1,13 +1,19 @@
 
 import json
-new_characters = []
+new_characters = [ 
+                  ]
 
 def add_charcters():
-    with open("character.json", "rw") as f:
+    with open("character.json", "r") as f:
         char_json = json.load(f)
-        for i in characters:
+    for i in new_characters:
             new_dict = {"name": i[0], "origin": i[1], "groups": i[1:]}
             char_json.append(new_dict)
+    with open("character.json", "w") as f:
         json.dump(char_json, f)
-with open("character.json", "r") as f:
-    characters = json.load(f)
+    print("charcters added")
+def characters():
+    with open("character.json", "r") as f:
+        return json.load(f)
+
+# add_charcters()
